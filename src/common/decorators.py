d8be97785081fc1b324705f22cc52b9853dd611a
DanilaKorobkov <k.danila67@gmail.com>
2019-03-17 11:00:42 +0300

    Рефакториг декораторов и путей к проекту

--- src/common/decorators.py
+++ src/common/decorators.py
@@ -3,31 +3,6 @@
 # Python
 import inspect
 
-def singleton(cls):
-    """
-    A singleton decorator. Returns a wrapper objects. A call on that object
-    returns a single instance object of decorated class. Use the __wrapped__
-    attribute to access decorated class directly in unit tests
-    """
-    return _SingletonWrapper(cls)
-
-
-class _SingletonWrapper:
-    """
-    A singleton wrapper class. Its instances would be created
-    for each decorated class.
-    """
-
-    def __init__(self, cls):
-        self.__wrapped__ = cls
-        self._instance = None
-
-    def __call__(self, *args, **kwargs):
-        """Returns a single instance of decorated class"""
-        if self._instance is None:
-            self._instance = self.__wrapped__(*args, **kwargs)
-        return self._instance
-
 
 def override(func):
     return func
@@ -49,3 +24,23 @@ def private(func):
         return func(*args, **kwargs)
 
     return func_wrapper
+
+
+def singleton(cls):
+
+    class _SingletonWrapper:
+
+        def __init__(self, classObject):
+
+            self.__wrapped__ = classObject
+            self._instance = None
+
+
+        def __call__(self, *args, **kwargs):
+
+            if self._instance is None:
+                self._instance = self.__wrapped__(*args, **kwargs)
+
+            return self._instance
+
+    return _SingletonWrapper(cls)
--- src/common/project_paths.py
+++ src/common/project_paths.py
@@ -1,25 +1,12 @@
-"""Пути к разным папкам проекта"""
-
 # Python
+import os
 
-import os, sys
-
-def __getSlash():
-
-    return os.path.join('1', '2')[1]
 
 projectName = 'EventRegistry'
-
-slash = __getSlash()
 
 executableScriptDir = os.path.abspath(os.curdir)
 
 pathToProject = executableScriptDir[: executableScriptDir.index(projectName) + len(projectName)]
 
 pathToSrc = os.path.join(pathToProject, 'src')
-
-pathToProject = pathToSrc[: pathToSrc.rfind(slash)]
-
-pathToConfig = pathToSrc
-
 pathToRuntimeGeneratedDialects = os.path.join(pathToSrc, 'domain', 'mavlink', 'runtime_dialects')
